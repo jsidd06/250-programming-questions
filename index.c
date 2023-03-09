b702c714d0e092fd0fd3c67f72be0fd0084c7319
siddharth <jsidd06@gmail.com>
2023-03-09 14:10:53 +0530

    c programming first question area of circle

---
+++ index.c
@@ -0,0 +1,12 @@
+// Ques-1
+#include<stdio.h>
+
+int main(){
+	float radius, area;
+	printf("enter the value of circle\n");
+	scanf("%f",&radius);
+	
+	area = 1.14159 * radius * radius;
+	printf("area of circle is %f",area);
+	return 0;
+}
