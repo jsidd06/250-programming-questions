7801544ad9625e3b44b199fb291920db4b497e58
siddharth <jsidd06@gmail.com>
2023-03-09 15:54:03 +0530

    add now both are 3 questions again

--- index.c
+++ index.c
@@ -1,3 +1,29 @@
+// Ques-1 AREA OF CIRCLE
+#include<stdio.h>
+
+int main(){
+	float radius, area;
+	printf("enter the value of circle\n");
+	scanf("%f",&radius);
+
+	area = 1.14159 * radius * radius;
+	printf("area of circle is %f",area);
+	return 0;
+}
+
+// Ques: 2 ASCII VALUE
+#include<stdio.h>
+
+int main() {
+	char c;
+	printf("enter a character: ");
+	scanf("%c",&c);
+
+	printf("ASCII of value %c =%d",c,c);
+	return 0;
+}
+
+// QUES 3 AREA OF TRIANGLE
 #include<stdio.h>
 
 int main() {
