bb705d91cc094bcb1dbd795a2827930c2c746d77
siddharth <jsidd06@gmail.com>
2023-03-10 11:45:42 +0530

    q9 The Display Size of the Different Data Types

--- index.c
+++ index.c
@@ -94,3 +94,16 @@ int main(){
 	printf("this is your temperature %f",FahrenheitS);
 	return 0;
 }
+
+QUES 9 The Display Size of the Different Data Types
+#include <stdio.h>
+
+int main() {
+  printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
+  printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
+  printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
+  printf("Size of Double Data Types in C = %2d bytes \n", sizeof(double));
+  printf("Size of Long Double Data Types in C = %2d bytes \n", sizeof(long double));
+  printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+  return 0;
+}
