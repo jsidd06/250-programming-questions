50d23a881908fef8993319effce74a0e96ebb0dd
siddharth <jsidd06@gmail.com>
2023-03-10 11:50:11 +0530

    q10 Factorial of a Given Number

--- index.c
+++ index.c
@@ -107,3 +107,25 @@ int main() {
   printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
   return 0;
 }
+
+QUES 10 Factorial of a Given Number
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // shows error if the user enters a negative integer
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
+
+    return 0;
+}
