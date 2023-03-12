7a5510eb32d9e3d29ac006193eff4c735f36248e
siddharth <jsidd06@gmail.com>
2023-03-12 10:52:25 +0530

    q9 leap year

--- if_else_statement_questions.c
+++ if_else_statement_questions.c
@@ -124,3 +124,24 @@ int main(){
     }
     return 0;
 }
+
+QUES 9 Leap Year?
+#include <stdio.h>
+int main() {
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
+   if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else {
+      printf("%d is not a leap year.", year);
+   }
+   return 0;
+}
