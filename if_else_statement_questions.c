8f93871208c2eae1bc0c943e6fbba817bc5bf0f3
siddharth <jsidd06@gmail.com>
2023-03-14 12:15:57 +0530

    q24 Count the total number of notes in a given amount

--- if_else_statement_questions.c
+++ if_else_statement_questions.c
@@ -400,3 +400,50 @@ int main(){
     }
     return 0;
 }
+
+QUES 24 Count the total number of notes in a given amount?
+#include<stdio.h>
+int main(){
+    int amount;
+    int note2000, note500, note200, note100, note50, note20, note10;
+    note2000 = note500 = note200 = note100 = note50 = note20 = note10 = 0;
+    printf("enter a amount:-");
+    scanf("%d",&amount);
+    if(amount >= 2000){
+        note2000 = amount/2000;
+        amount -= note2000 * 2000;
+    }
+     if(amount >= 500){
+        note500 = amount/500;
+        amount -= note500 * 500;
+    } 
+    if(amount >= 200){
+        note200 = amount/200;
+        amount -= note200 * 200;
+    } 
+    if(amount >= 100){
+        note100 = amount/100;
+        amount -= note100 * 100;
+    }
+     if(amount >= 50){
+        note50 = amount/50;
+        amount -= note50 * 50;
+    } 
+     if(amount >= 20){
+        note20 = amount/20;
+        amount -= note20 * 20;
+    } 
+     if(amount >= 10){
+        note10 = amount/10;
+        amount -= note10 * 10;
+    }
+    printf("Total number of notes = \n");
+    printf("2000 = %d\n", note2000);
+    printf("500 = %d\n", note500);
+    printf("200 = %d\n", note200);
+    printf("100 = %d\n", note100);
+    printf("50 = %d\n", note50);
+    printf("20 = %d\n", note20);
+    printf("10 = %d\n", note10);
+    return 0;
+}
